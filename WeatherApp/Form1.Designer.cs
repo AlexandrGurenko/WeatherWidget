6505870f42df6291ad501ce40c0470ce461cf7e2
AlexandrGurenko <gurenko.alexandr@gmail.com>
2018-04-20 11:54:10 +0300

    Initial commit

---
+++ WeatherApp/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WeatherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cityBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.findBtn = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cityBox
+            // 
+            this.cityBox.FormattingEnabled = true;
+            this.cityBox.Items.AddRange(new object[] {
+            "Винница",
+            "Днепр",
+            "Донецк",
+            "Житомир",
+            "Запорожье",
+            "Киев",
+            "Кропивницкий",
+            "Луганск",
+            "Луцк",
+            "Львов",
+            "Николаев",
+            "Одесса",
+            "Полтава",
+            "Ровно",
+            "Севастополь",
+            "Симферополь",
+            "Сумы",
+            "Тернополь",
+            "Ужгород",
+            "Харьков",
+            "Херсон",
+            "Хмельницкий",
+            "Черкассы",
+            "Чернигов",
+            "Черновцы"});
+            this.cityBox.Location = new System.Drawing.Point(12, 29);
+            this.cityBox.Name = "cityBox";
+            this.cityBox.Size = new System.Drawing.Size(121, 21);
+            this.cityBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Город";
+            // 
+            // findBtn
+            // 
+            this.findBtn.Location = new System.Drawing.Point(169, 9);
+            this.findBtn.Name = "findBtn";
+            this.findBtn.Size = new System.Drawing.Size(194, 41);
+            this.findBtn.TabIndex = 2;
+            this.findBtn.Text = "Показать погоду!";
+            this.findBtn.UseVisualStyleBackColor = true;
+            this.findBtn.Click += new System.EventHandler(this.findBtn_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputBox.Location = new System.Drawing.Point(16, 58);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(369, 554);
+            this.outputBox.TabIndex = 3;
+            this.outputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 624);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.findBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cityBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Погодный информер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cityBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button findBtn;
+        private System.Windows.Forms.TextBox outputBox;
+    }
+}
+
---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
+using System.Web;
+using System.Net;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> cities;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cityBox.Text = "Киев";
+
+            cities = new Dictionary<string, string>
+            {
+                {"Винница",     "http://rp5.ua/rss/187/ru" },
+                {"Днепр",       "http://rp5.ua/rss/247/ru" },
+                {"Донецк",      "http://rp5.ua/rss/298/ru" },
+                {"Житомир",     "http://rp5.ua/rss/342/ru" },
+                {"Запорожье",   "http://rp5.ua/rss/383/ru" },
+                {"Киев",        "http://rp5.ua/rss/509938/ru" },
+                {"Кропивницкий","http://rp5.ua/rss/471/ru" },
+                {"Луганск",     "http://rp5.ua/rss/520/ru" },
+                {"Луцк",        "http://rp5.ua/rss/224/ru" },
+                {"Львов",       "http://rp5.ua/rss/557/ru" },
+                {"Николаев",    "http://rp5.ua/rss/585/ru" },
+                {"Одесса",      "http://rp5.ua/rss/614/ru" },
+                {"Полтава",     "http://rp5.ua/rss/644/ru" },
+                {"Ровно",       "http://rp5.ua/rss/665/ru" },
+                {"Севастополь", "http://rp5.ua/rss/234/ru" },
+                {"Симферополь", "http://rp5.ua/rss/174/ru" },
+                {"Сумы",        "http://rp5.ua/rss/687/ru" },
+                {"Тернополь",   "http://rp5.ua/rss/712/ru" },
+                {"Ужгород",     "http://rp5.ua/rss/373/ru" },
+                {"Харьков",     "http://rp5.ua/rss/741/ru" },
+                {"Херсон",      "http://rp5.ua/rss/764/ru" },
+                {"Хмельницкий", "http://rp5.ua/rss/787/ru" },
+                {"Черкассы",    "http://rp5.ua/rss/811/ru" },
+                {"Чернигов",    "http://rp5.ua/rss/838/ru" },
+                {"Черновцы",    "http://rp5.ua/rss/853/ru" }
+            };
+        }
+
+        private void findBtn_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest query = null;
+            HttpWebResponse answer = null;
+
+
+            foreach (var i in cities)
+            {
+                if (i.Key == cityBox.Text)
+                {
+                    query = (HttpWebRequest)HttpWebRequest.Create(i.Value);
+                    break;
+                }
+            }
+
+            answer = (HttpWebResponse)query.GetResponse();
+            Stream content = answer.GetResponseStream();
+            StreamReader sr = new StreamReader(content);
+            string code = sr.ReadToEnd();
+
+            string path = @"data.xml";
+            FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(code);
+
+            sw.Close();
+            sr.Close();
+
+            XmlDocument xD = new XmlDocument();
+            xD.Load(path);
+            XmlElement root = xD.DocumentElement;
+            foreach (XmlNode xnode in root)
+            {
+                if (xnode.Name == "entry")
+                {
+                    foreach (XmlNode xnd in xnode.ChildNodes)
+                    {
+                        if (xnd.Name == "title")
+                        {
+                            outputBox.Text = xnd.InnerText;
+                            outputBox.Text += ",";
+                        }
+                        if (xnd.Name == "summary")
+                            outputBox.Text += xnd.InnerText;
+                    }
+                }
+            }
+        }
+    }
+}
